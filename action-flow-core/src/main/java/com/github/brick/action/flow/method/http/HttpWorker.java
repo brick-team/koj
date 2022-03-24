e6ad32f7cc7f7b93b924334dbaf41cd84fb8d8bb
huifer <huifer97@163.com>
2022-03-24 20:26:25 +0800

    fix: ok http 实现

--- action-flow-core/src/main/java/com/github/brick/action/flow/method/http/HttpWorker.java
+++ action-flow-core/src/main/java/com/github/brick/action/flow/method/http/HttpWorker.java
@@ -1,8 +1,9 @@
 package com.github.brick.action.flow.method.http;
 
+import java.io.IOException;
 import java.util.Map;
 
 public interface HttpWorker {
-    String work(String url, String method, Map<String, String> headers, Map<String, String> formatData, Map<String, String> body);
+    String work(String url, String method, Map<String, String> headers, Map<String, String> formatData, Map<String, String> body) throws IOException;
 
 }
--- action-flow-core/src/main/java/com/github/brick/action/flow/method/http/OkHttpWorkerImpl.java
+++ action-flow-core/src/main/java/com/github/brick/action/flow/method/http/OkHttpWorkerImpl.java
@@ -1,10 +1,110 @@
 package com.github.brick.action.flow.method.http;
 
+import com.google.gson.Gson;
+import okhttp3.*;
+
+import java.io.IOException;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 public class OkHttpWorkerImpl implements HttpWorker {
+
+    private static final OkHttpClient okHttpClient = new OkHttpClient().newBuilder()
+            .retryOnConnectionFailure(false).connectionPool(new ConnectionPool(50, 5, TimeUnit.MINUTES))
+            .connectTimeout(5, TimeUnit.SECONDS).readTimeout(5, TimeUnit.SECONDS).writeTimeout(5, TimeUnit.SECONDS)
+            .build();
+
+    private static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+    Gson gson = new Gson();
+
     @Override
-    public String work(String url, String method, Map<String, String> headers, Map<String, String> formatData, Map<String, String> body) {
-        return null;
+    public String work(String url, String method, Map<String, String> headers, Map<String, String> formatData, Map<String, String> body) throws IOException {
+        FormBody formBody = buildFormBody(formatData);
+
+        Headers reqHead = buildHeaders(headers);
+
+
+        if ("get".equalsIgnoreCase(method)) {
+            Request.Builder builder = new Request.Builder().url(url).headers(reqHead).get();
+            Request request = builder.build();
+            return getHttpResponse(request);
+
+        } else if ("post".equalsIgnoreCase(method)) {
+            if (!formatData.isEmpty()) {
+                Request request = new Request.Builder().url(url).post(formBody).headers(reqHead).build();
+                return getHttpResponse(request);
+            }
+
+            if (!body.isEmpty()) {
+                RequestBody requestBody = RequestBody.create(JSON, gson.toJson(body));
+                Request request = new Request.Builder()
+                        .url(url)
+                        .post(requestBody)
+                        .build();
+                return getHttpResponse(request);
+            }
+            throw new IllegalArgumentException("post 请求异常");
+
+        } else if ("put".equalsIgnoreCase(method)) {
+            if (!formatData.isEmpty()) {
+                Request request = new Request.Builder().url(url).put(formBody).headers(reqHead).build();
+                return getHttpResponse(request);
+            }
+
+            if (!body.isEmpty()) {
+                RequestBody requestBody = RequestBody.create(JSON, gson.toJson(body));
+                Request request = new Request.Builder()
+                        .url(url)
+                        .put(requestBody)
+                        .build();
+                return getHttpResponse(request);
+            }
+            throw new IllegalArgumentException("put 请求异常");
+
+        } else if ("delete".equalsIgnoreCase(method)) {
+            if (!formatData.isEmpty()) {
+                Request request = new Request.Builder().url(url).delete(formBody).headers(reqHead).build();
+                return getHttpResponse(request);
+            }
+
+            if (!body.isEmpty()) {
+                RequestBody requestBody = RequestBody.create(JSON, gson.toJson(body));
+                Request request = new Request.Builder()
+                        .url(url)
+                        .delete(requestBody)
+                        .build();
+                return getHttpResponse(request);
+            }
+            throw new IllegalArgumentException("put 请求异常");
+        } else {
+            throw new IllegalArgumentException("无法发送非 get post put delete 以外的请求");
+        }
+
+    }
+
+    private String getHttpResponse(Request request) throws IOException {
+        Call call = okHttpClient.newCall(request);
+        Response execute = call.execute();
+        assert execute.body() != null;
+        return execute.body().string();
+    }
+
+    private FormBody buildFormBody(Map<String, String> formatData) {
+        FormBody.Builder builder = new FormBody.Builder();
+        if (formatData.isEmpty()) {
+            return builder.build();
+        }
+        formatData.forEach(builder::add);
+        return builder.build();
+    }
+
+    private Headers buildHeaders(Map<String, String> headers) {
+        Headers.Builder builder = new Headers.Builder();
+        if (headers.isEmpty()) {
+            return builder.build();
+        }
+
+        headers.forEach(builder::add);
+        return builder.build();
     }
 }
