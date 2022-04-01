7507ec9f5ce340d931bf38a9a2a488fade4f7b19
hiABandon <hiabandon@outlook.com>
2022-04-01 22:21:14 +0800

    使用vue

---
+++ action-flow-web/action-flow-ui/src/main.js
@@ -0,0 +1,16 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import { ElButton, ElContainer, ElAside, ElMain, ElMenu, ElMenuItem, ElMenuItemGroup, ElSubMenu } from 'element-plus'
+import 'element-plus/dist/index.css'
+
+
+const elementPlusComponents = [
+    ElButton, ElContainer, ElAside, ElMain, ElMenu, ElMenuItem, ElSubMenu, ElMenuItemGroup
+]
+
+const app = createApp(App)
+elementPlusComponents.forEach(component => {
+    app.component(component.name, component)
+})
+app.use(router).mount('#app')
---
+++ action-flow-web/action-flow-ui/src/views/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <el-container>
+      <el-aside width="200px">
+        <el-menu router :default-active="$route.path">
+          <template v-for="m,index in menus" :key="index" :index="m.path">
+            <el-menu-item
+              :index="m.path"
+            >
+              <span>{{m.name}}</span>
+            </el-menu-item>
+          </template>
+        </el-menu>
+      </el-aside>
+        <router-view></router-view>
+    </el-container>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      menus: [
+        {name: '菜单1', path: '1'},
+        {name: '菜单2', path: '2'},
+        {name: '菜单3', path: '3'},
+      ]
+    }
+  }
+}
+</script>
