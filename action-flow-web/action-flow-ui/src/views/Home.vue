49f4fc2c192c81387a8be062ed1134177d2ddb43
Zen Huifer <huifer97@163.com>
2022-04-20 17:23:47 +0800

    fix: antv g6

--- action-flow-web/action-flow-ui/src/views/Home.vue
+++ action-flow-web/action-flow-ui/src/views/Home.vue
@@ -2,17 +2,16 @@
   <div>
 
     <el-button @click="hhhh">aaa</el-button>
-    <div id="mountNode"></div>
 
-
+    <div id="mountNode" />
   </div>
 </template>
 
 <script>
-import G6 from '@antv/g6';
+import G6 from '@antv/g6'
 
 export default {
-  name: 'polyline',
+  name: 'Polyline1',
   data() {
     return {
       graph: {},
@@ -35,96 +34,103 @@ export default {
           target: 'node2',
           source: 'node1'
         }]
-      }
-      ,
-      tooltip: "",
+      },
+      tooltip: '',
       top: 0,
-      left: 0, addedCount: 0,
+      left: 0,
+      addedCount: 0
 
-    };
+    }
+  },
+  mounted() {
+    this.$nextTick(() => {
+      this.ff()
+      this.createGraphic()
+      this.initGraphEvent()
+      console.log(this.graph)
+    })
   },
   methods: {
     hhhh() {
-      this.graph.setMode("addNode");
+      this.graph.setMode('addNode')
     },
     ff() {
-      console.log("ff");
+      const _this = this
+      console.log('ff')
       G6.registerBehavior('click-add-edge', {
         getEvents() {
           return {
             'node:click': 'onClick',
             mousemove: 'onMousemove',
             'edge:click': 'onEdgeClick' // 点击空白处，取消边
-          };
+          }
         },
         onClick(ev) {
-          const node = ev.item;
-          const graph = this.graph;
+          console.log(123)
+          const node = ev.item
+          const graph = this.graph
           const point = {
             x: ev.x,
             y: ev.y
-          };
-          const model = node.getModel();
+          }
+          const model = node.getModel()
           if (this.addingEdge && this.edge) {
             graph.updateItem(this.edge, {
               target: model.id
-            });
+            })
             // graph.setItemState(this.edge, 'selected', true);
-            this.edge = null;
-            this.addingEdge = false;
+            this.edge = null
+            this.addingEdge = false
           } else {
             this.edge = graph.addItem('edge', {
               source: model.id,
               target: point
-            });
-            this.addingEdge = true;
+            })
+            this.addingEdge = true
           }
         },
         onMousemove(ev) {
           const point = {
             x: ev.x,
             y: ev.y
-          };
+          }
           if (this.addingEdge && this.edge) {
             this.graph.updateItem(this.edge, {
               target: point
-            });
+            })
           }
         },
         onEdgeClick(ev) {
-          const currentEdge = ev.item;
+          const currentEdge = ev.item
           // 拖拽过程中，点击会点击到新增的边上
           if (this.addingEdge && this.edge == currentEdge) {
-            this.graph.removeItem(this.edge);
-            this.edge = null;
-            this.addingEdge = false;
+            graph.removeItem(this.edge)
+            this.edge = null
+            this.addingEdge = false
           }
         }
-      });
+      })
 
       // Register a custom behavior to add node
       G6.registerBehavior('click-add-node', {
         getEvents() {
           return {
             'canvas:click': 'onClick'
-          };
+          }
         },
         addedCount: 0,
         onClick(ev) {
-
-          const graph = this.graph;
+          const graph = this.graph
           const node = this.graph.addItem('node', {
             x: ev.canvasX,
             y: ev.canvasY,
-            id: `node-${this.addedCount}`, // 生成唯一的 id
-          });
-          this.addedCount++;
+            id: `node-${this.addedCount}` // 生成唯一的 id
+          })
+          this.addedCount++
         }
-      });
+      })
     },
     createGraphic() {
-
-
       this.graph = new G6.Graph({
         container: 'mountNode',
         width: 500,
@@ -143,23 +149,15 @@ export default {
             fill: 'steelblue'
           }
         }
-      });
+      })
 
-      this.graph.data(this.data);
-      this.graph.render();
+      this.graph.data(this.data)
+      this.graph.render()
     },
     initGraphEvent() {
-    },
-  },
-  mounted() {
-    this.$nextTick(() => {
-      this.ff();
-      this.createGraphic();
-      this.initGraphEvent();
-      console.log(this.graph);
-    });
+    }
   }
-};
+}
 </script>
 
 <style>
