b724b164e8e4e40d8ca4b8fc197fa93472f5456f
Zen Huifer <huifer97@163.com>
2022-04-21 15:54:56 +0800

    fix: antv g6

--- action-flow-web/action-flow-ui/src/main.js
+++ action-flow-web/action-flow-ui/src/main.js
@@ -1,10 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import { ElButton, ElContainer, ElAside, ElMain, ElMenu, ElMenuItem, ElMenuItemGroup, ElSubMenu } from 'element-plus'
+import { ElButton, ElContainer, ElAside, ElMain, ElMenu, ElMenuItem, ElMenuItemGroup, ElSubMenu ,ElDialog} from 'element-plus'
 import 'element-plus/dist/index.css'
+
 const elementPlusComponents = [
-    ElButton, ElContainer, ElAside, ElMain, ElMenu, ElMenuItem, ElSubMenu, ElMenuItemGroup
+    ElButton, ElContainer, ElAside, ElMain, ElMenu, ElMenuItem, ElSubMenu, ElMenuItemGroup,ElDialog
 ]
 
 const app = createApp(App)
