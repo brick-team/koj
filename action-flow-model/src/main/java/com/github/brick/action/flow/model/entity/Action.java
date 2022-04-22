1164c67920cf191ca8d0db511907de9316df8f1c
Simon <1196182913@qq.com>
2022-04-22 14:57:16 +0800

    fix: 增加file_name

--- action-flow-model/src/main/java/com/github/brick/action/flow/model/entity/Action.java
+++ action-flow-model/src/main/java/com/github/brick/action/flow/model/entity/Action.java
@@ -1,3 +1,19 @@
+/*
+ *    Copyright [2022] [brick-team]
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
 package com.github.brick.action.flow.model.entity;
 
 import lombok.Data;
@@ -31,5 +47,9 @@ public class Action{
     * type=2时表示需要执行的类全路径
     */
     private String className;
+    /**
+     * 文件名称
+     */
+    private String fileName;
 
 }
